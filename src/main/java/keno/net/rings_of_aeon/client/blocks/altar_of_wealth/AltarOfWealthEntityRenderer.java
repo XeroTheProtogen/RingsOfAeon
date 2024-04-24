3a01991ea3797825e77d11c9083eb30baaa4ffb5
db318 <db318330@gmail.com>
2024-04-24 12:17:00 +1200

    Finished Renderer + did some bug fixes

--- src/main/java/keno/net/rings_of_aeon/client/blocks/altar_of_wealth/AltarOfWealthEntityRenderer.java
+++ src/main/java/keno/net/rings_of_aeon/client/blocks/altar_of_wealth/AltarOfWealthEntityRenderer.java
@@ -2,6 +2,8 @@ package keno.net.rings_of_aeon.client.blocks.altar_of_wealth;
 
 import keno.net.rings_of_aeon.blocks.AltarOfWealthBlock;
 import keno.net.rings_of_aeon.blocks.entities.AltarOfWealthTileEntity;
+import keno.net.rings_of_aeon.util.ItemRenderingHelper;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.OverlayTexture;
@@ -16,8 +18,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RotationAxis;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 
-public class AltarOfWealthEntityRenderer implements BlockEntityRenderer<AltarOfWealthTileEntity> {
+public class AltarOfWealthEntityRenderer implements BlockEntityRenderer<AltarOfWealthTileEntity>, ItemRenderingHelper {
     public AltarOfWealthEntityRenderer(BlockEntityRendererFactory.Context context) {
     }
 
@@ -25,12 +29,40 @@ public class AltarOfWealthEntityRenderer implements BlockEntityRenderer<AltarOfW
     public void render(AltarOfWealthTileEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
         ItemStack renderStack = entity.getStack(0);
-        
+        BlockState state = entity.getCachedState();
+        for (int i = 1; i < state.get(AltarOfWealthBlock.ITEMS) + 1; i++) {
+            //Pass as y for translation
+            float center = 0.85f;
+            Vector3f scaling = new Vector3f(0.4f, 0.4f, 0.4f);
+            switch (i) {
+                case 1 -> handleItemTransformation(matrices, new Vector3f(0.689f, center, 0.5f),
+                        scaling, RotationAxis.NEGATIVE_X.rotationDegrees(28), i);
+                case 2 -> handleItemTransformation(matrices, new Vector3f(0.5f, center, 0.689f),
+                        scaling, RotationAxis.POSITIVE_X.rotationDegrees(28), i);
+                case 3 -> handleItemTransformation(matrices, new Vector3f(0.313f, center, 0.5f),
+                        scaling, RotationAxis.NEGATIVE_X.rotationDegrees(28), i);
+                case 4 -> handleItemTransformation(matrices, new Vector3f(0.5f, center, 0.313f),
+                        scaling, RotationAxis.POSITIVE_X.rotationDegrees(28), i);
+            }
+            itemRenderer.renderItem(renderStack, ModelTransformationMode.GUI, getLightLevel(entity.getWorld(),
+                    entity.getPos()), OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, entity.getWorld(), 1);
+            matrices.pop();
+        }
     }
+
+
 
     private int getLightLevel(World world, BlockPos pos) {
         int bBright = world.getLightLevel(LightType.BLOCK ,pos);
         int sBright = world.getLightLevel(LightType.SKY, pos);
         return LightmapTextureManager.pack(bBright, sBright);
+    }
+
+    @Override
+    public void handleItemTransformation(MatrixStack matrices, Vector3f translation, Vector3f scale, Quaternionf rotation,int i) {
+        matrices.push();
+        ItemRenderingHelper.super.handleItemTransformation(matrices, translation, scale, rotation, i);
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(90 * i));
+        matrices.multiply(rotation);
     }
 }
--- src/main/java/keno/net/rings_of_aeon/items/TabletOfWealthItem.java
+++ src/main/java/keno/net/rings_of_aeon/items/TabletOfWealthItem.java
@@ -1,15 +1,19 @@
 package keno.net.rings_of_aeon.items;
 
+import keno.net.rings_of_aeon.blocks.AltarOfWealthBlock;
 import keno.net.rings_of_aeon.effects.RCStatusEffects;
+import keno.net.rings_of_aeon.registries.RCCommonRegistry;
 import keno.net.rings_of_aeon.util.TimeConversion;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class TabletOfWealthItem extends Item {
@@ -20,28 +24,35 @@ public class TabletOfWealthItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (!world.isClient()) {
-            if (user.getMainHandStack().getItem() == this) {
-                applyEffect(user);
+            Vec3d playerCameraPos = user.getCameraPosVec(user.age);
+            BlockState playerHighlightedBlock = world.getBlockState(new BlockPos(
+                    (int) playerCameraPos.getX(),
+                    (int) playerCameraPos.getY(),
+                    (int) playerCameraPos.getZ()));
+            if (user.getStackInHand(hand).getItem() == this &&
+                    (playerHighlightedBlock.getBlock() == RCCommonRegistry.ALTAR_OF_WEALTH
+                            && playerHighlightedBlock.get(AltarOfWealthBlock.ITEMS) == 4)) {
+                applyEffect(user, hand);
             }
         }
         return TypedActionResult.pass(user.getStackInHand(hand));
     }
 
-    private void applyEffect(PlayerEntity user) {
+    private void applyEffect(PlayerEntity user, Hand hand) {
         StatusEffect wealthy = RCStatusEffects.WEALTHY;
         if (!user.hasStatusEffect(wealthy)) {
             StatusEffectInstance wealthInstance = new StatusEffectInstance(wealthy, TimeConversion.secondsToTicks(30));
             user.addStatusEffect(wealthInstance);
             user.getItemCooldownManager().set(this, TimeConversion.secondsToTicks(2));
+            user.getStackInHand(hand).decrement(1);
+
         } else if (user.hasStatusEffect(wealthy) && user.getStatusEffect(wealthy).getAmplifier() < 3) {
             StatusEffectInstance previousInstance = user.getStatusEffect(wealthy);
             StatusEffectInstance currentInstance = new StatusEffectInstance(previousInstance.getEffectType(),
                     previousInstance.getDuration() + TimeConversion.secondsToTicks(30), previousInstance.getAmplifier() + 1);
             user.addStatusEffect(currentInstance);
             user.getItemCooldownManager().set(this, TimeConversion.secondsToTicks(1));
-        }
-        if (user.hasStatusEffect(wealthy) && user.getStatusEffect(wealthy).getAmplifier() < 3) {
-            user.getMainHandStack().decrement(1);
+            user.getStackInHand(hand).decrement(1);
         }
     }
 }
